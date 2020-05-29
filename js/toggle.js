6744d0c14099042c5f641ecef0e778fb28253dfc
edavid78 <61856383+edavid78@users.noreply.github.com>
2020-05-29 14:22:24 -0400

    Complete

---
+++ js/toggle.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    var body = document.body;
+    var header = document.getElementById("header");
+
+    body.classList.toggle("dark-mode");
+    header.classList.toggle("header-dark-mode");
+ }
